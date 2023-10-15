41ae0a38e7a0cc3bdfc147c5f148dd3d3ab417fc
김동혁 <clfgnm9@naver.com>
2023-10-15 13:10:05 +0900

    14-01 props-children

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   const client = new ApolloClient({
     uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), //컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시로 저장해놓기
+    cache: new InMemoryCache(), // 컴퓨터의 메모리에 백엔드에서 받아온 데이터 임시로 저장해놓기
   });
 
   return (
---
+++ pages/section14/14-01-props.children/index.tsx
@@ -0,0 +1,13 @@
+import Example from "../../../src/components/units/14-props-children-example";
+
+export default function PropsChildrenPage(): JSX.Element {
+  return (
+    <Example school="다람쥐초등학교">
+      <div>
+        <input type="text" />
+        <div>저는 철수입니다.</div>
+        <button>클릭해주세요</button>
+      </div>
+    </Example>
+  );
+}
