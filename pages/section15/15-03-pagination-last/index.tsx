7ffb7b87ce227d2d85b5f68482b0e5407ccc0575
김동혁 <clfgnm9@naver.com>
2023-11-29 20:13:07 +0900

    15-04-lifting-state-up

--- pages/section15/15-03-pagination-last/index.tsx
+++ pages/section15/15-03-pagination-last/index.tsx
@@ -1,40 +1,47 @@
 import { gql, useQuery } from "@apollo/client";
-import type{ IQuery, IQueryFetchBoardsArgs, IQueryFetchBoardsCountArgs } from "../../../src/commons/types/generated/types";
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+  IQueryFetchBoardsCountArgs,
+} from "../../../src/commons/types/generated/types";
 import type { MouseEvent } from "react";
 import { useState } from "react";
 
 const FETCH_BOARDS = gql`
-  query fetchBoards($page: Int){
+  query fetchBoards($page: Int) {
     fetchBoards(page: $page) {
       _id
       writer
       title
       contents
     }
-  } 
+  }
 `;
 
 const FETCH_BOARDS_COUNT = gql`
-  query{
+  query {
     fetchBoardsCount
   }
-`
+`;
 
 export default function StaticRoutingMovePage(): JSX.Element {
-  const [startPage, setStartPage] = useState(1)
-  const { data, refetch } = useQuery<Pick<IQuery, "fetchBoards">, IQueryFetchBoardsArgs>(FETCH_BOARDS);
+  const [startPage, setStartPage] = useState(1);
+  const { data, refetch } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
 
   //  게시물 전체갯수 불러오기
   const { data: dataBoardsCount } = useQuery<
     Pick<IQuery, "fetchBoardsCount">,
     IQueryFetchBoardsCountArgs
   >(FETCH_BOARDS_COUNT);
-  
+
   const lastPage = Math.ceil((dataBoardsCount?.fetchBoardsCount ?? 10) / 10);
 
   const onClickPage = (event: MouseEvent<HTMLSpanElement>): void => {
-    void refetch({ page: Number(event.currentTarget.id) })
-  }
+    void refetch({ page: Number(event.currentTarget.id) });
+  };
   // const onClickPage2 = ():void => {
   //   void refetch({ page: 2 })
   // }
@@ -44,17 +51,17 @@ export default function StaticRoutingMovePage(): JSX.Element {
 
   const onClickPrevPage = (): void => {
     if (startPage === 1) return;
-    setStartPage(startPage-10)
-    void refetch({page: startPage-10})
-  }
+    setStartPage(startPage - 10);
+    void refetch({ page: startPage - 10 });
+  };
   const onClickNextPage = (): void => {
     if (startPage + 10 <= lastPage) {
-      setStartPage(startPage+10)
+      setStartPage(startPage + 10);
       void refetch({
-      page: startPage+10})  
+        page: startPage + 10,
+      });
     }
-    
-  }
+  };
 
   return (
     <>
@@ -66,20 +73,20 @@ export default function StaticRoutingMovePage(): JSX.Element {
       ))}
 
       <span onClick={onClickPrevPage}>이전페이지</span>
-      {
-        new Array(10).fill(1).map((_, index) => 
-
-          index + startPage <= lastPage &&
-          (<span
-            key={index + startPage}
-            id={String(index + startPage)}
-            onClick={onClickPage}
-            style={{ margin: "5px" }}
-          >
-            {index + startPage}</span>)
-        )}
+      {new Array(10).fill(1).map(
+        (_, index) =>
+          index + startPage <= lastPage && (
+            <span
+              key={index + startPage}
+              id={String(index + startPage)}
+              onClick={onClickPage}
+              style={{ margin: "5px" }}
+            >
+              {index + startPage}
+            </span>
+          ),
+      )}
       <span onClick={onClickNextPage}>다음페이지</span>
-
 
       {/* {
         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((_, index) => (
