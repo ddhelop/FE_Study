0e66bb15343104cb67d127c5a5054a0d1afef839
김동혁 <clfgnm9@naver.com>
2023-12-29 19:16:25 +0900

    image board

---
+++ pages/section19/19-03-image-validation/index.tsx
@@ -0,0 +1,63 @@
+import { gql, useMutation } from "@apollo/client";
+import { useState, type ChangeEvent, useRef } from "react";
+import {
+  IMutation,
+  IMutationUploadFileArgs,
+} from "../../../src/commons/types/generated/types";
+import { CheckValidation } from "../../../src/commons/libraries/validationFile";
+
+const UPLOAD_FILE = gql`
+  mutation uploadFile($file: Upload!) {
+    uploadFile(file: $file) {
+      url
+    }
+  }
+`;
+
+export default function ImageUploadPage(): JSX.Element {
+  const [imageUrl, setImageUrl] = useState("");
+  const fileRef = useRef<HTMLInputElement>(null);
+
+  const [uploadFile] = useMutation<
+    Pick<IMutation, "uploadFile">,
+    IMutationUploadFileArgs
+  >(UPLOAD_FILE);
+
+  const onChangeFile = async (
+    event: ChangeEvent<HTMLInputElement>,
+  ): Promise<void> => {
+    const file = event.target.files?.[0]; // 배열로 들어오는 이유: <input type="file" multiple />일 때 여러개 드래그 가능
+    console.log(file);
+
+    const isValid = CheckValidation(file);
+    if (!isValid) return;
+
+    const result = await uploadFile({ variables: { file } });
+    console.log(result.data?.uploadFile.url);
+    setImageUrl(result.data?.uploadFile.url ?? "");
+  };
+
+  const onClickImage = (): void => {
+    // document.getElementById("파일태그ID")?.click();
+    fileRef.current?.click();
+  };
+
+  return (
+    <>
+      <div
+        style={{ width: "100px", height: "100px", background: "gray" }}
+        onClick={onClickImage}
+      >
+        이미지선택
+      </div>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        onChange={onChangeFile}
+        ref={fileRef}
+        accept="image/jpeg, image/png" // 자체적으로 막는방법
+      />
+      <img src={`https://storage.googleapis.com/${imageUrl}`} />
+    </>
+  );
+}
