8e46b9e0702b41efe14425cf286c045cf12a121c
김동혁 <clfgnm9@naver.com>
2024-01-08 22:23:22 +0900

    react-hook-from-with-yup

---
+++ pages/section24/24-02-react-hook-form-with-yup/02-after.tsx
@@ -0,0 +1,47 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "./02-after.validation";
+import { wrapAsync } from "../../../src/commons/libraries/asyncFunc";
+
+interface IFormData {
+  writer: string;
+  title: string;
+  contents: string;
+  email: string;
+  password: string;
+  phone: string;
+  // boardAddress: {
+  //   addressDetail: string;
+  // };
+}
+
+export default function GraphqlMutationPage(): JSX.Element {
+  const { register, handleSubmit, formState } = useForm<IFormData>({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+  const onClickSubmit = (data: IFormData): void => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={wrapAsync(handleSubmit(onClickSubmit))}>
+      작성자 :<input type="text" {...register("writer")} />
+      <div style={{ color: "red" }}>{formState.errors.writer?.message}</div>
+      제목 : <input type="text" {...register("title")} />
+      <div style={{ color: "red" }}>{formState.errors.title?.message}</div>
+      내용 : <input type="text" {...register("contents")} />
+      <div style={{ color: "red" }}>{formState.errors.contents?.message}</div>
+      {/* 주소 : <input type="text" {...register("boardAddress.addressDetail")} /> */}
+      <button style={{ backgroundColor: formState.isValid ? "yellow" : "" }}>
+        GRAPHQL-API 요청하기
+      </button>
+    </form>
+  );
+}
+
+/*
+  <button type="reset">지우자</button>
+  <button type="submit">등록하자</button> // 기본값
+  <button type="button" onClick={onClickBasket}> // 버튼만 실행할때
+*/
